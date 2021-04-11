0c9756d5ab9654856d0adbcae0cd61b871f1612f
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-11 10:16:18 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ let num1=2;
 let num2=31;
 let multiply=num1*num2;
 
-let random=Math.random()+0.01;
-let num3=16;
-let num4=3;
+let random=Math.ceil(Math.random());
+
+let num3=256;
+let num4=6;
 let mod=num3%num4;
 let max=Math.max(1,2,20);
